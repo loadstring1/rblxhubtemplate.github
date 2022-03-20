7c2c654b83ce4c9d718a0342d4ad4b6004ae200f
loadstring1 <97308997+loadstring1@users.noreply.github.com>
2022-03-20 22:57:44 +0100

    Update 5199264352.lua

--- games/5199264352.lua
+++ games/5199264352.lua
@@ -1,7 +1,7 @@
 local request = http_request or request or HttpPost or syn.request
 local MainSystem = loadstring(request({Url = "https://raw.githubusercontent.com/loadstring1/rblxhubtemplate.github/main/UI.lua",Method = "GET"}).Body)
 if typeof(MainSystem) == "function" then MainSystem = MainSystem() end
-local lib = MainSystem:CreateUIlib()
+local lib = MainSystem:CreateUILib()
 lib:CreateUI()
 lib:ParentAndProtectUI(nil,true)
 task.wait(0.6)
