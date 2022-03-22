694edf86844285b74058c80e1f65d9cc335b3f36
loadstring1 <97308997+loadstring1@users.noreply.github.com>
2022-03-22 18:12:09 +0100

    Delete games directory

--- games/5199264352.lua
+++
@@ -1,8 +0,0 @@
-local request = http_request or request or HttpPost or syn.request
-local MainSystem = loadstring(request({Url = "https://raw.githubusercontent.com/loadstring1/rblxhubtemplate.github/main/UI.lua",Method = "GET"}).Body)
-if typeof(MainSystem) == "function" then MainSystem = MainSystem() end
-local lib = MainSystem:CreateUILib()
-lib:CreateUI()
-lib:ParentAndProtectUI(nil,true)
-task.wait(0.6)
-lib:DestroyUI()
